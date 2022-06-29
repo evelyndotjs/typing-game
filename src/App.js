ed051d63d29d58f4de71b206930eed46b829a618
Evelyn Huang <evelyn@Evelyns-MacBook-Pro.local>
2022-06-30 01:22:11 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  const STARTING_TIME = 5;
+
+  const [userInput, setUserInput] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
+
+  function handleChange(e) {
+    setUserInput(e.target.value);
+  }
+
+  function calcWordCount(text) {
+    const arr = text.trim().split(" ");
+    return arr.filter((word) => word !== "").length;
+  }
+
+  function startClock() {
+    setIsTimeRunning(true);
+    setTimeRemaining(STARTING_TIME);
+    setUserInput("");
+    setWordCount(0);
+  }
+
+  function endGame() {
+    setIsTimeRunning(false);
+    setWordCount(calcWordCount(userInput));
+  }
+
+  useEffect(() => {
+    if (isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((time) => time - 1);
+      }, 1000);
+    } else if (timeRemaining === 0) {
+      endGame();
+    }
+  }, [timeRemaining, isTimeRunning]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>How fast can you type?</h1>
+      <textarea
+        disabled={!isTimeRunning}
+        onChange={handleChange}
+        type="text"
+        value={userInput}
+      />
+      <h4>Time remaining: {timeRemaining}s</h4>
+      <button disabled={isTimeRunning} onClick={startClock}>
+        Start
+      </button>
+      <h1>Word count: {wordCount} </h1>
     </div>
   );
 }
-
-export default App;
